b92bfdc5dd8510eb5ef5af23377c1cfa39926f38
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-06-12 21:00:00 -0400

    debugging

--- LogAnalysisConsoleApp/Program.cs
+++ LogAnalysisConsoleApp/Program.cs
@@ -1,10 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
 Console.WriteLine(LogAnalysis.SubstringAfter(" am the 1st test", " "));
 Console.ReadKey();
 
+Console.WriteLine(LogAnalysis.SubstringBetween("FIND >>> SOMETHING <===< HERE", ">>> ", " <===<"));
 
+Console.ReadLine();
+
+Console.WriteLine(LogAnalysis.SubstringBetween("[INFO]: File Deleted.", "[", "]"));
+
+Console.ReadLine();
 public static class LogAnalysis
 {
     public static string SubstringAfter(this string str, string delimeter)
@@ -18,9 +23,15 @@ public static class LogAnalysis
         string del2 = delimeter2.Trim();
         string trimmedString = str.Replace(" ", "");
 
-        Console.WriteLine(trimmedString.Length);
+        Console.WriteLine($"Length of the string to find {trimmedString.Length}");
+
+        Console.WriteLine("first delimeter, length of first delimiter, second delimiter, length of second delimter");
         Console.WriteLine(delimeter1 + " " + delimeter1.Trim().Length + " " + delimeter2 + " " + delimeter2.Trim().Length);
-        Console.WriteLine(str.IndexOf(delimeter1) + " " + delimeter1.Trim().Length + " " + str.IndexOf(delimeter2) + " " + delimeter2.Trim().Length);
+
+        Console.WriteLine("index of first delimeter, kength of first delimeter, index of second delimeter, length of second delimeter");
+        Console.WriteLine(trimmedString.IndexOf(delimeter1) + " " + delimeter1.Trim().Length + " " + trimmedString.IndexOf(delimeter2) + " " + delimeter2.Trim().Length);
+
+        Console.WriteLine("Trimmed string:");
         Console.WriteLine(trimmedString);
 
         return trimmedString.Substring(trimmedString.IndexOf(del1) + del1.Length, trimmedString.IndexOf(del2) - del2.Length);
@@ -33,9 +44,4 @@ public static class LogAnalysis
     {
         return SubstringBetween(str, "[", "]");
     }
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-
-    // TODO: define the 'Message()' extension method on the `string` type
-
-    // TODO: define the 'LogLevel()' extension method on the `string` type
 }
